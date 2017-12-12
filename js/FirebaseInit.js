84496d3e212dece814cf41f40743adb435a4cd38
ichinator <lazonegeek1@gmail.com>
2017-12-12 15:47:49 +0100

    first commit

---
+++ js/FirebaseInit.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+	  var config = {
+	    apiKey: "AIzaSyAaCE4QvfvjTiXtQg05f9HccIXRxF0XSSE",
+	    authDomain: "monapp-7d6a4.firebaseapp.com",
+	    databaseURL: "https://monapp-7d6a4.firebaseio.com",
+	    projectId: "monapp-7d6a4",
+	    storageBucket: "monapp-7d6a4.appspot.com",
+	    messagingSenderId: "691884489498"
+	  };
+	  firebase.initializeApp(config);
